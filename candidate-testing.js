fcca33bf815190a55af4302eccb49b64133660f7
River Kahrhoff <river.kahrhoff@gmail.com>
2024-06-27 17:22:34 -0500

    complete assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,9 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?" , "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["SALLY RIDE", "TRUE", "40", "TRAJECTORY", "3"];
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -26,7 +26,7 @@ function askQuestion() {
 for (let i = 0; i < questions.length; i++) {
   candidateAnswer = (input.question(questions[i]));
   candidateAnswers.push(candidateAnswer.toUpperCase());
-  if (candidateAnswers[i] === correctAnswers[i]) {
+  if (candidateAnswers[i] === correctAnswers[i].toUpperCase()) {
     console.log("CORRECT");
   } else {
     console.log(`INCORRECT\nCorrect Answer: ${correctAnswers[i]}`)
@@ -56,7 +56,7 @@ function gradeQuiz(candidateAnswers) {
   let totalNumQuestions = correctAnswers.length;
     
   for (let i = 0; i < correctAnswers.length; i++) {
-    if (correctAnswers[i] === candidateAnswers[i]) {
+    if (correctAnswers[i].toUpperCase() === candidateAnswers[i]) {
        numCorrectAnswers += 1    
     }
   }
