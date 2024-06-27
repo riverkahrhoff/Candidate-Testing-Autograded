70f6cd87fc4b3adc857b84e3b2b8ad80dd23d69b
River Kahrhoff <river.kahrhoff@gmail.com>
2024-06-27 13:44:13 -0500

    part 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,8 +11,8 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?" , "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["SALLY RIDE", "TRUE", "40", "TRAJECTORY", "3"];
 let candidateAnswers;
 
 
@@ -23,7 +23,15 @@ candidateName = input.question("Please enter your name: ")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+for (let i = 0; i < questions.length; i++) {
+  candidateAnswer = (input.question(questions[i]));
+  candidateAnswers.push(candidateAnswer.toUpperCase());
+  if (candidateAnswers[i] === correctAnswers[i]) {
+    console.log("CORRECT");
+  } else {
+    console.log(`INCORRECT\nCorrect Answer: ${correctAnswers[i]}`)
+  }
+}
 
 }
 
@@ -31,14 +39,38 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-if (candidateAnswer === correctAnswer) {
-  console.log("Correct!")
-} else {
-  console.log("Incorrect!")
-};
+  console.log(`
+    ----------
+    
+    Candidate Name: ${candidateName}`)
+    for (let i = 0; i < questions.length; i++) {
+      console.log(`
+    Question ${questions[i].trim()}
+      Your answer: ${candidateAnswers[i]}
+      Correct answer: ${correctAnswers[i]}`)
+    }
+
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
+  let numCorrectAnswers = 0
+  let totalNumQuestions = correctAnswers.length;
+    
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (correctAnswers[i] === candidateAnswers[i]) {
+       numCorrectAnswers += 1    
+    }
+  }
+   grade = numCorrectAnswers/totalNumQuestions * 100
+  
+    console.log(`
+    
+  >>> Overall Grade: ${grade}% (${numCorrectAnswers} of ${totalNumQuestions} responses correct) <<<`)
+  
+  if (grade >= 80) {
+    console.log(`>>> Status: PASSED <<<`);
+  } else {
+    console.log(`>>> Status: FAILED <<<`);
+  }
 
   return grade;
 }
@@ -62,4 +94,4 @@ module.exports = {
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
-};
+};  
